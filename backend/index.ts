f3e446b10bce2057f65b4dd8633b370ea1109bfb
RahafAl0 <ra.al9760@gmail.com>
2023-08-17 18:44:06 +0300

    No greeting for anonymous!

--- backend/index.ts
+++ backend/index.ts
@@ -1,28 +1,27 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
-
-
 const app: Express = express();
-const port = '3000' || process.env.PORT;
+const port = "3000" || process.env.PORT;
 
-app.use(cors())
+app.use(cors());
 
+app.get("/", (req: Request, res: Response) => {
+  const username: any = req.query.username;
 
-app.get('/', (req: Request, res: Response) => {
-  const username = req.query.username;
-   
-  if (username === "Rahaf"){
-    return  res.json({ message: 'Hello Boss ' + username});
-
-  }else{
-   res.json({ message: 'Hello ' + username});
-
+  if (!username){
+    return res.json({ message: "No greeting for anonymous!" })
   }
+
+  if (username === "Rahaf") {
+    return res.json({ message: "Hello Boss " + username });
+  } else {
+    res.json({ message: "Hello " + username });
+  }
+ 
 });
 
 app.listen(port, () => {
