e5adcf3c38a36966293d967044b012e802e62640
RahafAl0 <ra.al9760@gmail.com>
2023-08-21 00:33:50 +0300

    new route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.tsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element : <App/>
+  }
+  ,{
+    path: "/messages",
+    element: <div> 
+      <h1>Messges!</h1>
+    </div>,
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
